72cfa268f265e6178865cc0ba4302ccfc62e265b
David Molina <david.molina@qaracter.com>
2025-12-22 12:25:07 +0100

    Added Hand class and its logic

---
+++ src/main/java/model/Hand.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+
+    private final List<Card> cards;
+    private int handValue;
+
+    public Hand() {
+        cards = new ArrayList<>();
+    }
+
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+    
+    public void calculateValue() {
+        int value = 0;
+        int aceCount = 0;
+
+        for (Card card : cards) {
+            value += card.getValue();
+            if (card.getRank() == Rank.ACE) { //Need to know how many ACEs we have
+                aceCount++;
+            }
+        }
+
+        // If the hand value is more than 21 then an ACE should count as 1 instead of 11
+        while (value > 21 && aceCount > 0) {
+            value -= 10;
+            aceCount--;
+        }
+
+        ;
+    }
+
+
+    public boolean isBlackjack() {
+        return cards.size() == 2 && this.handValue == 21;
+    }
+
+    public boolean hasLost() {
+        return this.handValue > 21;
+    }
+
+    public int getHandValue(){
+        return handValue;
+    }
+
+
+    public void showHand(boolean hideFirstCard) {
+        for (int i = 0; i < cards.size(); i++) {
+            if (i == 0 && hideFirstCard) {
+                System.out.println("[Hidden card]");
+            } else {
+                System.out.println(cards.get(i));
+            }
+        }
+
+        if (!hideFirstCard) {
+            System.out.println("Total value: " + calculateValue());
+        }
+    }
+
+
+    public void clear() {
+        cards.clear();
+    }
+}
